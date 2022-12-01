70e5014a3a4a71f778f20945018d6f0eecc159d0
whydigo <ukunsss7@gmail.com>
2022-12-01 15:17:26 +0300

    теперь можно добавлять теги, ура

--- src/App.js
+++ src/App.js
@@ -21,9 +21,41 @@ function App() {
     setSended(false)
   }
 
+  const addTags = () => {
+    setTag([
+      ...tag,
+      {
+        text: text
+      }
+    ]);
+    setText('')
+  }
+
+  const handleDelete = (i) => {
+    const filtered = tag.filter((tag, index) => {
+      if (index === i) {
+        return false
+      } else {
+        return true
+      }
+    })
+    setTag(filtered)
+  }
+
+
   const [blur, setBlur] = useState(false)
   const [sended, setSended] = useState(false)
-
+  const [tag, setTag] = useState([
+    {
+      text: 'JavaScript'
+    },
+    {
+      text: 'TypeScript'
+    },
+    {
+      text: 'MySQL'
+    }
+  ])
 
   return (
     <div className="App">
@@ -33,12 +65,15 @@ function App() {
         onChange={handleSetInput} 
         onBlur={handleBlur} 
         className={!text && blur ? "error" : "norm"}/>
-        <button  type='submit' disabled={!text && true}>
+        <button  type='submit' disabled={!text && true} className={text ? 'sub' : 'notsub'} onClick={addTags}>
           Отправить
         </button>
       </form>
       {sended && <div className='text right'>Сообщение успешно отправлено</div>}
       {(!text && blur) && <div className='text wrong'>Поле ввода не должно быть пустым</div>}
+      <div className='tags'>
+      {tag.map((item, index) => <div className="tag">{item.text} <button className="dlt" onClick={() => {handleDelete(index)}}>x</button> </div>)}
+      </div>
     </div>
   );
 }
